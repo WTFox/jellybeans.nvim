520c81956b15cbe9dc6ccf015feccd4f3beea16a
A. Fox <anthonyfox1988@gmail.com>
2025-05-08 14:08:38 -0700

    feat: update config.lua to reflect new type

--- lua/jellybeans/config.lua
+++ lua/jellybeans/config.lua
@@ -1,8 +1,10 @@
 local M = {}
 
----@class jellybeans.Background
----@field dark string
----@field light string
+---@alias palette_name "jellybeans" | "jellybeans_light" | "jellybeans_muted" | "jellybeans_muted_light"
+
+---@class jellybeans.Config.Background
+---@field dark palette_name
+---@field light palette_name
 
 ---@class jellybeans.Config
 ---@field transparent? boolean
@@ -10,15 +12,15 @@ local M = {}
 ---@field flat_ui? boolean
 ---@field on_highlights? fun(highlights: jellybeans.Highlights, colors: ColorScheme)
 ---@field on_colors? fun(colors: ColorScheme)
----@field background? jellybeans.Background
+---@field background? jellybeans.Config.Background
 M.defaults = {
+  transparent = false,
+  italics = true,
+  flat_ui = true,
   background = {
     dark = "jellybeans_muted",
     light = "jellybeans_muted_light",
   },
-  transparent = false,
-  italics = true,
-  flat_ui = true,
   plugins = {
     all = false,
     auto = true,
@@ -29,6 +31,20 @@ M.defaults = {
 
 ---@param opts? jellybeans.Config
 function M.setup(opts)
+  if opts.style ~= nil then
+    vim.notify_once(
+      "[jellybeans.nvim] opts.style is deprecated, please use opts.background instead. Refer to README for usage.",
+      vim.log.levels.WARN
+    )
+  end
+
+  if opts.palette ~= nil then
+    vim.notify_once(
+      "[jellybeans.nvim] opts.palette is deprecated, please use opts.background instead. Refer to README for usage.",
+      vim.log.levels.WARN
+    )
+  end
+
   M.opts = M.extend(opts)
 end
 
